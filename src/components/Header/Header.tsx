6c41a4950172463395db6a4ea28b022c5856a853
glyvin <glyvin@pointcad1.onmicrosoft.com>
2022-10-17 08:20:05 +0300

    changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,18 @@
 import "./Header.css"
 import logo from "../../images/logo.svg"
+import Link from "../Link/Link"
 
 export function Header() {
   return (
     <header className="header">
+      <img src={logo} className="logo" alt="Логотип"/>
       <nav className="header__nav">
-            <img src={logo} className="logo" alt="Логотип"/>
+            
       </nav>
+      <div className="header__profile">
+        <Link title="Регистрация" url="/" mod=""/>
+        <Link title="Войти" url="/" mod=""/>
+      </div>
     </header>
   )
 }
--- src/components/Promo/Promo.tsx
+++ src/components/Promo/Promo.tsx
@@ -1,9 +1,14 @@
 import "./Promo.css"
+import logo from "../../images/landing-logo.svg"
 
 export function Promo() {
   return (
-    <div className="promo">
+    <section className="promo">
+      <h1 className="promo__title">
+        Учебный проект студента факультета Веб-разработки.
+      </h1>
+            <img src={logo} className="promo__logo" alt="Глобус из надписей"/>
 
-    </div>
+    </section>
   )
 }
