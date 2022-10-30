9f4a56ac6e75183b0595acfbc18e7ee6a2748a62
Alexey glyvin <glyvin@pointcad.ru>
2022-10-30 21:53:15 +0300

    changes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,17 @@
 import { Main } from "../Main/Main"
+import { Movies } from "../mo"
 import React from "react";
-import { Route, Switch, useHistory, useLocation, Redirect } from "react-router-dom";
+import { Route, Routes, BrowserRouter, useHistory, useLocation, Redirect } from "react-router-dom";
 import "./App.css"
 
 function App() {
   return (
     <div className="app">
-        <Switch>
-          <Route exact path="/">
-            <Main/>
-          </Route>
-        </Switch>
+        <Routes>
+          <Route exact path="/" element={<Main />}/>
+          <Route exact path="/movies" element={<Movies />}/>
+
+        </Routes>
     </div>
   );
 }
